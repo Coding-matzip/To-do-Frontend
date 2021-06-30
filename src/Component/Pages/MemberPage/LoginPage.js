69c5a91a0e50a5140a7dc3b67b7181f03c018e1a
Harim Jeong <harim97714@naver.com>
2021-06-30 16:18:16 +0900

    LoginPage, SignupPage 기본적인 틀 구현

--- src/Component/Pages/MemberPage/LoginPage.js
+++ src/Component/Pages/MemberPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { siteTitle } from '../../Config';
+import './MemberPage.css';
 
 function LoginPage() {
     useEffect(() => {
@@ -9,7 +10,27 @@ function LoginPage() {
     return (
         <div>
             <section>
-                LoginPage
+                <div id="main-content">
+                    <div id="content-name">Login</div>
+                    <form method="post">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Email</td>
+                                    <td><input type="email" name="email" required /></td>
+                                </tr>
+                                <tr>
+                                    <td>Password</td>
+                                    <td><input type="password" name="password" required /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div id="button-area">
+                            <span><button className="login" type="submit">Sign Up</button></span>
+                            <span><button className="login" type="submit">Log In</button></span>
+                        </div>
+                    </form>
+                </div>
             </section>
         </div>
     )
--- src/Component/Pages/MemberPage/SignupPage.js
+++ src/Component/Pages/MemberPage/SignupPage.js
@@ -9,7 +9,30 @@ function SignupPage() {
     return (
         <div>
             <section>
-                SignupPage
+                <div id="main-content">
+                    <div id="content-name">Sign Up</div>
+                    <form method="post">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Email</td>
+                                    <td><input type="email" name="email" required /></td>
+                                </tr>
+                                <tr>
+                                    <td>Password</td>
+                                    <td><input type="password" name="password" required /></td>
+                                </tr>
+                                <tr>
+                                    <td>Confirm Password</td>
+                                    <td><input type="password" name="password" required /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div id="button-area">
+                            <span><button className="signup" type="submit">Register</button></span>
+                        </div>
+                    </form>
+                </div>
             </section>
         </div>
     )
