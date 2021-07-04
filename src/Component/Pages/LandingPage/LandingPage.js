9c791a6ce9a4c3f53ca6c5f97791139b12d5fda3
DASHorRUSH <67368023+DASHorRUSH@users.noreply.github.com>
2021-07-05 00:00:21 +0900

    07.05 미완성

--- src/Component/Pages/LandingPage/LandingPage.js
+++ src/Component/Pages/LandingPage/LandingPage.js
@@ -1,18 +1,94 @@
-import React, { useEffect } from 'react';
-import { siteTitle } from '../../Config';
+import React, { useState, useCallback, useRef } from "react";
+import TodoMain from "./components/TodoMain";
+import TodoList from "./components/TodoList";
+import TodoInsert from "./components/TodoInsert";
+import logoutIcon from "./image/icon-logout.svg";
+import homeIcon from "./image/icon-main.svg";
+import scheduleIcon from "./image/icon-check-last-schedule.svg";
+import TodoPage from "./components/TodoPage";
+
+
+const App = () => {
+    const [todos, setTodos] = useState([
+      {
+        id: 1,
+        text: "방울이랑 놀아주기",
+        checked: false,
+      },
+      {
+        id: 2,
+        text: "교수님과 즐겁고 행복하게 졸업작품 만들기",
+        checked: true,
+      },
+      {
+        id: 3,
+        text: "로또 1등 당첨되기",
+        checked: false,
+      },
+    ]);
+
+    const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
 
 function LandingPage() {
-    useEffect(() => {
-        document.title = siteTitle;  //Title 변경.
-    })
+  useEffect(() => {
+    document.title = siteTitle; //Title 변경.
+  });
 
-    return (
-        <div>
-            <section>
-                LandingPage
-            </section>
-        </div>
-    )
+  return (
+    <div>
+      <section>
+        <TodoMain>
+          <TodoInsert onInsert={onInsert} />
+          <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+          <TodoPage />
+        </TodoMain>
+        <nav>
+          <ul id="menu">
+            <li key="logout">
+              <img src={logoutIcon} alt="logout" />
+            </li>
+            <li className="active" key="home">
+              <img src={homeIcon} alt="home" />
+            </li>
+            <li key="schedule">
+              <img src={scheduleIcon} alt="schedule" />
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </div>
+  );
 }
 
 export default LandingPage;
