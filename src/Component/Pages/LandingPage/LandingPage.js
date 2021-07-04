e8cb875b6f07fa15c24c9cb0373702cc7a1150e1
DASHorRUSH <67368023+DASHorRUSH@users.noreply.github.com>
2021-07-05 00:14:33 +0900

    07.05 -- 00:14

--- src/Component/Pages/LandingPage/LandingPage.js
+++ src/Component/Pages/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import TodoMain from "./components/TodoMain";
 import TodoList from "./components/TodoList";
 import TodoInsert from "./components/TodoInsert";
@@ -6,28 +6,32 @@ import logoutIcon from "./image/icon-logout.svg";
 import homeIcon from "./image/icon-main.svg";
 import scheduleIcon from "./image/icon-check-last-schedule.svg";
 import TodoPage from "./components/TodoPage";
+import { siteTitle } from "../../Config";
 
+function LandingPage() {
+  useEffect(() => {
+    document.title = siteTitle; //Title 변경.
+  });
 
-const App = () => {
-    const [todos, setTodos] = useState([
-      {
-        id: 1,
-        text: "방울이랑 놀아주기",
-        checked: false,
-      },
-      {
-        id: 2,
-        text: "교수님과 즐겁고 행복하게 졸업작품 만들기",
-        checked: true,
-      },
-      {
-        id: 3,
-        text: "로또 1등 당첨되기",
-        checked: false,
-      },
-    ]);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "방울이랑 놀아주기",
+      checked: false,
+    },
+    {
+      id: 2,
+      text: "교수님과 즐겁고 행복하게 졸업작품 만들기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "로또 1등 당첨되기",
+      checked: false,
+    },
+  ]);
 
-    const nextId = useRef(4);
+  const nextId = useRef(4);
 
   const onInsert = useCallback(
     (text) => {
@@ -60,33 +64,26 @@ const App = () => {
     [todos]
   );
 
-function LandingPage() {
-  useEffect(() => {
-    document.title = siteTitle; //Title 변경.
-  });
-
   return (
     <div>
-      <section>
-        <TodoMain>
-          <TodoInsert onInsert={onInsert} />
-          <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
-          <TodoPage />
-        </TodoMain>
-        <nav>
-          <ul id="menu">
-            <li key="logout">
-              <img src={logoutIcon} alt="logout" />
-            </li>
-            <li className="active" key="home">
-              <img src={homeIcon} alt="home" />
-            </li>
-            <li key="schedule">
-              <img src={scheduleIcon} alt="schedule" />
-            </li>
-          </ul>
-        </nav>
-      </section>
+      <TodoMain>
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+        <TodoPage />
+      </TodoMain>
+      <nav>
+        <ul id="menu">
+          <li key="logout">
+            <img src={logoutIcon} alt="logout" />
+          </li>
+          <li className="active" key="home">
+            <img src={homeIcon} alt="home" />
+          </li>
+          <li key="schedule">
+            <img src={scheduleIcon} alt="schedule" />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
