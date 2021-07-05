775e4ced3cb98b151c5cfa4ad5b49ab7dc9ca876
Harim Jeong <harim97714@naver.com>
2021-07-05 15:05:06 +0900

    회원가입 페이지 개선
    비밀번호 입력시 6자리 이상 20자리 이하로 강제.
    placeholder를 사용해서 안내 문구 출력하도록 개선.

--- src/Component/Pages/MemberPage/SignupPage.js
+++ src/Component/Pages/MemberPage/SignupPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { siteTitle } from '../../Config';
 import './MemberPage.css';
-import kakaoLoginButton from '../../../image/kakao_login_medium_narrow.png';
 
 function SignupPage() {
     useEffect(() => {
@@ -16,15 +15,15 @@ function SignupPage() {
                     <form method="post">
                         <div className="input-area">
                             <div className="element label"><label for="email_field">Email</label></div>
-                            <div className="element"><input type="email" name="email" id="email_field" required></input></div>
+                            <div className="element"><input type="email" name="email" placeholder="이메일을 형식에 맞게 입력해주세요." id="email_field" required></input></div>
                         </div>
                         <div className="input-area">
                             <div className="element label"><label for="password_field">Password</label></div>
-                            <div className="element"><input type="password" name="password" id="password_field" required></input></div>
+                            <div className="element"><input type="password" name="password" id="password_field" placeholder="6자리 이상 20자리 이하의 비밀번호를 입력해주세요." pattern="^([a-z0-9_]){6,20}$" required></input></div>
                         </div>
                         <div className="input-area">
                             <div className="element label"><label for="confirm_password_field">Confirm Password</label></div>
-                            <div className="element"><input type="password" name="confirmPassword" id="confirm_password_field" required></input></div>
+                            <div className="element"><input type="password" name="confirmPassword" id="confirm_password_field" placeholder="비밀번호를 한 번 더 입력해주세요." pattern="^([a-z0-9_]){6,20}$" required></input></div>
                         </div>
                         <div id="button-area">
                             <span><button className="signup" type="submit">Register</button></span>
